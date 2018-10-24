70579c9770f89292a85092858aba61be0931f780
Whooolia <tpdms2779@gmail.com>
2018-10-24 10:59:24 +0100

    calc

--- main.js
+++ main.js
@@ -10,42 +10,35 @@ function init() {
             
             lastInput = display.textContent.slice(-1);
             currentInput = e.currentTarget.value;
-          
-            
+   
             // To not display operators in a row
             if(isNaN(parseInt(lastInput)) === false || isNaN(parseInt(currentInput)) === false) {
                 display.textContent += e.currentTarget.value;
+                
             }
             console.log(display.textContent);
 
             // because there are no decimals YET!
+
+            var operators = ['+','-','x','/'];
+            var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
             if(e.currentTarget.value === '.'){
-                if(decimalPointExist){
+                if(!decimalPointExist){
                     display.textConetent += e.currentTarget.value;
                     decimalPointExist = true;
-                } else if(!decimalPointExist){
-                    for(var i = 0; i < display.textContent.length; i++){
-                        if(display.textContent[i] === '+' || display.textContent[i] === '-' ||display.textContent[i] === 'X' || display.textContent[i] === '/'){
-                            
-                        } else {
+                } else {
+                        if(isNaN(parseInt(lastInput)) === true && e.currentTarget.value === (isNaN(parseInt(lastInput)) === false)){
                             display.textConetent += e.currentTarget.value;
+                        } else if(isNaN(parseInt(lastInput)) === false && e.currentTarget.value === (isNaN(parseInt(lastInput)) === false)){
+                            display.textConetent += e.currentTarget.value;
+                        } else{
+                            console.log('should not pring the dot');
                         }
-                    }
-                       
-                    }
-                    
                 }
-            })
-        };
-            
-            
-
-        
-        }
-   
-
-    // No repeat decimal
-   
+            }; 
+        })
+    }
+}
        
 
 
