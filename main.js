8a97af8b9bc577b9453852c661f3ea531b5ce9b7
Whooolia <tpdms2779@gmail.com>
2018-10-27 15:38:50 +0100

    calc

--- main.js
+++ main.js
@@ -4,41 +4,45 @@ var currentInput;
 function init() {
     var buttons = document.querySelectorAll("#calc button");
     var decimalPointExist = false;
+    // Pring number + operators not allowed prining in a row
     for(var i = 0; i < buttons.length; i++){
         buttons[i].addEventListener("click", function (e) {
             var display = document.getElementById('display');
+            console.log(display.innerHTML);
             
             lastInput = display.textContent.slice(-1);
             currentInput = e.currentTarget.value;
+
+            // Remove the default 0 as soon as it starts 
+            if(display.textContent === '0'){
+                display.textContent = ' ';
+            }
    
             // To not display operators in a row
             if(isNaN(parseInt(lastInput)) === false || isNaN(parseInt(currentInput)) === false) {
                 display.textContent += e.currentTarget.value;
-                
             }
             console.log(display.textContent);
 
-            // because there are no decimals YET!
-
-            var operators = ['+','-','x','/'];
-            var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-            if(e.currentTarget.value === '.'){
+            // Problem - multiple '.' is not allowed
                 if(!decimalPointExist){
-                    display.textConetent += e.currentTarget.value;
+                    display.textContent += e.currentTarget.value;
                     decimalPointExist = true;
                 } else {
-                        if(isNaN(parseInt(lastInput)) === true && e.currentTarget.value === (isNaN(parseInt(lastInput)) === false)){
-                            display.textConetent += e.currentTarget.value;
-                        } else if(isNaN(parseInt(lastInput)) === false && e.currentTarget.value === (isNaN(parseInt(lastInput)) === false)){
-                            display.textConetent += e.currentTarget.value;
-                        } else{
-                            console.log('should not pring the dot');
-                        }
+                        if(isNaN(parseInt(lastInput)) === false && e.currentTarget.value === '.'){
+                            display.textContent += e.currentTarget.value = '';
+                            decimalPointExist = true;  
                 }
-            }; 
-        })
+
+            // Calc the number
+            // eval(display.textContent);
+            console.log(eval('2'+'2'));
+            
+            }
+        });
     }
 }
+
        
 
 
